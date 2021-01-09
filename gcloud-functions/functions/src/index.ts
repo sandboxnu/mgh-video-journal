a091fe3ddf4654411f0d5a7efcb20b4b64ae2a8d
Zefeng (Daniel) Wang <45447059+zefwang@users.noreply.github.com>
2021-01-08 19:24:02 -0500

    Adding Cloud function to send data from Cloud Storage to Dropbox (#15)
    * tried to fetch but the dropbox api is broken. for the dropbox token, contact me to get access.
    
    * add delete from firebase.
    
    * update node version
    
    * Updated workflow to get all dependencies
    
    Co-authored-by: Jack Rosen <jrosen081@gmail.com>

---
+++ gcloud-functions/functions/src/index.ts
@@ -0,0 +1,50 @@
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
+import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
+import fetch from "node-fetch";
+const token = require("./DO_NOT_ADD.json");
+
+if (admin.apps.length === 0) {
+  admin.initializeApp();
+}
+
+exports.exportDropbox = functions.storage
+  .object()
+  .onFinalize(async (object) => {
+    const filePath = object.name;
+    if (filePath == null) {
+      return;
+    }
+
+    const fileName = filePath.split("/");
+    if (fileName[0] === "extra-check") {
+      fileName.shift();
+      const fbFile = await admin.storage().bucket().file(filePath).download();
+      await fetch("https://content.dropboxapi.com/2/files/upload", {
+        body: Buffer.concat(fbFile),
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${token["not_the_access_token"]}`,
+          "Content-Type": "application/octet-stream",
+          "Dropbox-API-Arg": JSON.stringify({
+            path: "/" + fileName.join("/"),
+            mode: "add",
+            autorename: true,
+            strict_conflict: false,
+            mute: false,
+          }),
+        },
+      })
+        .then(console.log)
+        .catch(console.log);
+    }
+
+    await admin
+      .storage()
+      .bucket()
+      .file(filePath)
+      .delete()
+      .then(console.log)
+      .catch(console.log);
+  });
